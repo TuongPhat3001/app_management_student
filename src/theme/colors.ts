8a3fdbee5fc26e4d05e4dcf456665152f0c7a4b1
Truong Tuong Phat <phattt30012004@gmail.com>
2026-07-12 19:19:03 +0700

    .

---
+++ src/theme/colors.ts
@@ -0,0 +1,23 @@
+export default {
+  primary: "#3949FF",
+
+  secondary: "#E9ECFF",
+
+  success: "#28C76F",
+
+  danger: "#EA5455",
+
+  warning: "#FF9F43",
+
+  white: "#FFFFFF",
+
+  black: "#000000",
+
+  text: "#222222",
+
+  border: "#E5E7EB",
+
+  background: "#F7F8FC",
+
+  card: "#FFFFFF",
+};
